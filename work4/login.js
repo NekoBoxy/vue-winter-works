98dcfd904448a1363c41777b1157fc1aac34d067
CatBoxy <catboxy@gmail.com>
2023-02-02 11:20:32 +0800

    第四周初始

---
+++ work4/login.js
@@ -0,0 +1,45 @@
+// 遠端伺服器位址與 API path
+const url = "https://vue3-course-api.hexschool.io";
+const path = "catboxy";
+
+const app = {
+  data() {
+    return {
+      username: "",
+      password: "",
+    };
+  },
+  methods: {
+    // 登入
+    handleLogin() {
+      axios({
+        method: "post",
+        url: `${url}/v2/admin/signin`,
+        data: {
+          username: this.username,
+          password: this.password,
+        },
+      })
+        .then((response) => {
+          // 登入成功取 token & expired
+          // const token = response.data.token;
+          // const expired = response.data.expired;
+          const { token, expired } = response.data;
+          // console.log("token:", token);
+          // console.log("expired:", expired);
+          console.log(response.data.message);
+          // 將取得的 token & expired 存入 cookie
+          document.cookie = `hextoken=${token}; expires=${new Date(expired)}`;
+
+          location.href = "products.html";
+        })
+        .catch((error) => {
+          // 登入失敗取錯誤訊息
+          console.dir(error);
+          const errorMessage = error.response.data.message;
+          alert(errorMessage);
+        });
+    },
+  },
+};
+Vue.createApp(app).mount("#app");
